a825c1143e38a5e323e11aa5104c50c8d8aa41c7
lzalar <leonardo.zalar@tacta.io>
2024-04-03 21:24:13 +0200

    add groupExpressions

--- parser/parser.go
+++ parser/parser.go
@@ -57,6 +57,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -77,6 +78,17 @@ func (p *Parser) noPrefixParseFnError(t token.TokenType) {
 func (p *Parser) noInfixParseFnError(t token.TokenType) {
 	msg := fmt.Sprintf("no infix parse function for %s found", t)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	exp := p.parseExpression(LOWEST)
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return exp
 }
 
 func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
