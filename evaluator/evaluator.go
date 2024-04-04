9eac669b79b41d84218938a4a5df7251a2825e1f
lzalar <leonardo.zalar@tacta.io>
2024-04-04 22:46:21 +0200

    add bang evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -3,11 +3,13 @@ package evaluator
 import (
 	"monkey/ast"
 	"monkey/object"
+	"monkey/token"
 )
 
 var (
 	TRUE  = &object.Boolean{Value: true}
 	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
 )
 
 func Eval(node ast.Node) object.Object {
@@ -20,10 +22,34 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
-	panic("monkeybrain melted")
 	return nil
+}
+
+func evalPrefixExpression(operator string, object object.Object) object.Object {
+	switch operator {
+	case token.BANG:
+		return evalBangOperator(object)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperator(o object.Object) object.Object {
+	switch o {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func nativeBoolToBooleanObject(input bool) object.Object {
