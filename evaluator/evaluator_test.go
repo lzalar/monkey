f1f418329b6b8ddcf97267a8695e7758a8d82caf
lzalar <leonardo.zalar@tacta.io>
2024-04-19 20:01:21 +0200

    add puts test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -417,7 +417,7 @@ func TestBuiltInFunctions(t *testing.T) {
 		{`rest([])`, nil},
 		{`push([], 1)`, []int{1}},
 		{`push(1, 1)`, "argument to `push` must be ARRAY, got INTEGER"},
-		//{`puts("hello", "world!")`, nil},
+		{`puts("hello", "world!")`, nil},
 	}
 
 	for _, test := range tests {
