5374d71a59b7a7d69b0b744fa6f2ea1d1b75b7f7
lzalar <leonardo.zalar@tacta.io>
2024-04-19 16:50:05 +0200

    add builtin array functions

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -24,4 +24,80 @@ var builtins = map[string]*object.Builtin{
 			}
 		},
 	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return arg.Elements[0]
+				}
+				return NULL
+			default:
+				return newError("argument to `first` must be ARRAY, got %s", arg.Type())
+
+			}
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) > 0 {
+					return arg.Elements[len(arg.Elements)-1]
+				}
+				return NULL
+			default:
+				return newError("argument to `last` must be ARRAY, got %s", arg.Type())
+
+			}
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				length := len(arg.Elements)
+				if length > 0 {
+					arr := make([]object.Object, length-1)
+					copy(arr, arg.Elements[1:length])
+					return &object.Array{Elements: arr}
+				}
+				return NULL
+			default:
+				return newError("argument to `rest` must be ARRAY, got %s", arg.Type())
+
+			}
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.Array:
+				length := len(arg.Elements)
+
+				arr := make([]object.Object, length, length+1)
+				copy(arr, arg.Elements[:])
+				arr = append(arr, args[1])
+				return &object.Array{Elements: arr}
+			default:
+				return newError("argument to `push` must be ARRAY, got %s", arg.Type())
+			}
+		},
+	},
 }
